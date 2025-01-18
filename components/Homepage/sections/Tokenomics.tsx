b39f78e583e1e3afbb3aa348596be88b23d5ce58
TimiDev19 <rotimioluwatimilehin19@gmail.com>
2025-01-18 09:20:16 +0100

    minor updates

--- components/Homepage/sections/Tokenomics.tsx
+++ components/Homepage/sections/Tokenomics.tsx
@@ -5,13 +5,13 @@ import React from 'react'
 const Tokenomics = () => {
     return (
         <div id='tokenomics' className=' w-full px-[10px] min-h-[50vh] text-white flex flex-col items-center justify-center'>
-            <h1 className=' text-center lg:text-left text-white font-extrabold text-3xl mb-4 flex'>T<Typewriter text={`OKENOMICS`} speed={300} /> </h1>
+            <h1 className=' text-center lg:text-left text-white font-extrabold text-3xl mb-4 flex'>T<Typewriter text={`OKENOMICS`} speed={300}/> </h1>
             {/* <h1 className=' text-3xl mb-4'>Key Details</h1> */}
             <p className=' text-xl'>
-            • Total Supply: 420,000,000 MEG (fixed, no future mining). <br />
-            • Community Funds & Airdrops: 40,000,000 (9.5%). <br />
-            • Open Market: 380,000,000 (90.5%). <br />
-            • No Inflation: MEG's capped supply ensures scarcity and long-term value
+            • <span className=' font-semibold'>Total Supply:</span> 420,000,000 MEG (fixed, no future mining). <br />
+            • <span className="font-semibold">Community Funds & Airdrops:</span> 40,000,000 (9.5%). <br />
+            • <span className="font-semibold">Open Market:</span> 380,000,000 (90.5%). <br />
+            • <span className="font-semibold">No Inflation:</span> MEG's capped supply ensures scarcity and long-term value
             </p>
         </div>
     )
