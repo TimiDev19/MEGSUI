1af4327a77c49c0eaf6b1d4a9806521a18b1f63d
TimiDev19 <rotimioluwatimilehin19@gmail.com>
2025-07-12 22:16:44 +0100

    new art

--- components/Homepage/sections/NFT.tsx
+++ components/Homepage/sections/NFT.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
-import Logo from "@/public/images/img1.jpg"
+import Logo from "@/app/assets/new-logo-kamal.jpg"
 import nft1 from "@/app/assets/nft1.jpg"
 import nft2 from "@/app/assets/nft2.jpg"
 import nft3 from "@/app/assets/nft3.jpg"
@@ -42,16 +42,19 @@ const NFT = () => {
                             className=' w-full hidden lg:block rounded-md border border-white mb-4'
                         />
                     </div>
-                    <div className=' text-white lg:w-[30%] pt-[10px]'>
+                    <div className=' text-white lg:w-[30%] pt-[10px] flex flex-col items-center justify-center'>
                         <h1 className=' text-xl lg:text-5xl'>NFT Details</h1>
                         <ol className=' list-disc'>
                             <li>WL mint 4.20 SUI</li>
                             <li>Public mint 6.9 SUI</li>
                             <li>420 NFTs</li>
                             <li>24 1 of 1s</li>
-                            <li>$1000 worth of prizes</li>
+                            <li>Over $1000 in prizes</li>
                             <li>Exclusive access to the discord</li>
                         </ol>
+                        <h1 className=' z-[3]  mx-auto flex items-center justify-center font-bold text-black text-lg lg:text-2-xl p-2 bg-gradient-to-tr from-blue-700 to-blue-300 w-full lg:mx-0 lg:w-fit rounded-xl box-shadow'>
+                            Art by EdCriptoFi
+                        </h1>
                     </div>
                     <div className=' w-full lg:w-[30%] h-full flex flex-col items-center justify-between'>
                         <Image
