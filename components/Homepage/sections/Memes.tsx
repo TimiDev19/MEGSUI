e129e106ddebbc4fd10bf04519e784955cbcb25d
TimiDev19 <rotimioluwatimilehin19@gmail.com>
2025-07-13 22:47:41 +0100

    pic removed

--- components/Homepage/sections/Memes.tsx
+++ components/Homepage/sections/Memes.tsx
@@ -8,11 +8,6 @@ const Memes = () => {
         <div className=' w-[100vw]'>
             <div className=' w-[95%] mx-auto flex items-center justify-center mb-[20px]'>
                 <h1 className=' z-[3] flex items-center justify-center uppercase font-bold text-black text-lg lg:text-4xl p-2 bg-gradient-to-tr from-blue-700 to-blue-300 w-full mx-auto lg:mx-0 lg:w-fit rounded-xl box-shadow'>
-                    <Image
-                        src={Logo}
-                        alt="Apexium Logo"
-                        className=' w-[70px] h-[70px] rounded-full mr-3'
-                    />
                     MEGSUI MEMES
                 </h1>
                 <InfiniteCarousel />
